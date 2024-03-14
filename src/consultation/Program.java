a430e633cc272532024791a2be7fa0422efdd8e8
Oleksandr0130 <olek1995sta@gmail.com>
2024-03-14 17:30:06 +0100

    new

--- src/consultation/Program.java
+++ src/consultation/Program.java
@@ -2,7 +2,7 @@ package consultation;
 
 import java.util.Scanner;
 
-public class program {
+public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input value1: ");
