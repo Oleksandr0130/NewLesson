89d5ed3ecb452833a0371f46e1f535b5ff3263e6
Oleksandr0130 <olek1995sta@gmail.com>
2024-03-14 17:06:51 +0100

    new

---
+++ src/consultation/program.java
@@ -0,0 +1,26 @@
+package consultation;
+
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input value1: ");
+        double val1 = scanner.nextDouble();
+
+        System.out.println("Input value2: ");
+        int val2 = scanner.nextInt();
+
+        double result = 1;
+        if (val2 >= 0) {
+            for (int i = 0; i < val2; i++) {
+                result *= val1;
+            }
+        }else {
+            for (int i = 0; i > val2; i--) {
+                result /= val1;
+            }
+        }
+        System.out.println("result: " + result);
+    }
+}
