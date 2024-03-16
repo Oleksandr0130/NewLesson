63746118d3f79ba019e534fb5edcc5caae655b2a
Oleksandr0130 <olek1995sta@gmail.com>
2024-03-16 16:32:54 +0100

    new

---
+++ src/lesson_8/HomeWork/Task2.java
@@ -0,0 +1,39 @@
+package lesson_8.HomeWork;
+
+/*
+Допустим дан массив чисел.
+
+Написать метод, который возвращает самое маленькое число в этом массиве
+Написать метод, который возвращает индекс заданного числа в массиве.
+Если в массиве нет заданного числа, возвращаем -1,
+если заданное число встречается несколько раз,
+возвращаем первый индекс, где встретилось данное число.
+ */
+public class Task2 {
+
+    public static int minValue(int[] array) {
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int getIndexByValue(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 1, 3, 4, 5, 6, 7, 8, 9, -1, -1000};
+
+        System.out.println(minValue(array));
+        System.out.println(getIndexByValue(array, 1));
+    }
+}
